9f0cb3f5d606c242413f514b73dd1602981277dc
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-05-27 13:09:09 -0700

    simple webpack config with dev server

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 var Welcome = React.createClass({
   render: function(){
     return (
       <div>
-        <h1>Demo timez</h1>
+        <h1>Demo timez!</h1>
         <p>Let's count stuff</p>
       </div>
     )
