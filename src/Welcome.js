7af0f41e674112594c6ac388bcbcc4b616e603f0
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-05-27 13:12:57 -0700

    hot module replacement for react

---
+++ src/Welcome.js
@@ -0,0 +1,14 @@
+const React = require('react');
+
+var Welcome = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Demo timez</h1>
+        <p>Let's count stuff</p>
+      </div>
+    )
+  }
+})
+
+module.exports = Welcome;
--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-var Welcome = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <h1>Demo timez!</h1>
-        <p>Let's count stuff</p>
-      </div>
-    )
-  }
-})
+var Welcome = require('./Welcome');
 
 var Counter = React.createClass({
 
