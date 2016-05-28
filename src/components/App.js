803df007396337f9d125ed9666a02dbe2036953a
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-05-27 20:26:52 -0700

    pre-flux

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,6 @@ import NavBar from './NavBar'
 export default class App extends Component {
   constructor(props){
     super(props);
-
-    this.state = {
-      tenants: []
-    }
   }
 
   render() {
