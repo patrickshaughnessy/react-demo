6e98a27d46ad38bad9e747a7a1d6384c4060e589
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-07-25 13:07:38 -0700

    simplify first component

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
 var Root = React.createClass({
 
-  getInitialState: function(){
-    return {
-      count: 0
-    }
-  },
-
-  addCount: function(){
-    this.setState({ count: this.state.count + 1 })
-  },
-
-  minusCount: function(){
-    this.setState({ count: this.state.count - 1 })
-  },
-
   render: function(){
     return (
-      <div>
-        <h1>Demo timez</h1>
-        <p>Let's count stuff</p>
-
-        <button onClick={this.addCount}>+</button>
-        <button onClick={this.minusCount}>-</button>
-
-        <h3>Counter: {this.state.count}</h3>
-      </div>
+      <h1>Hello from React!</h1>
     )
   }
 })
