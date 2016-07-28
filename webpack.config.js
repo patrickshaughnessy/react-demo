e921c63507a14cc741054769a50901b043027773
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-07-27 21:59:43 -0700

    eslinting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import webpack from 'webpack'
-import path from 'path'
+import { join } from 'path'
+
 
 export default {
   debug: true,
@@ -7,35 +8,37 @@ export default {
   entry: [
     'webpack-hot-middleware/client?reload=true',
     'bootstrap-loader',
-    './src/index'
+    './src/index',
   ],
   target: 'web',
   output: {
-    path: __dirname + '/dist',
+    path: join(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
-    contentBase: './src'
+    contentBase: './src',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        include: path.join(__dirname, 'src'),
-        loader: 'babel'
+        include: join(__dirname, 'src'),
+        loader: 'babel',
       },
-      {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+      { test: /(\.css)$/, loaders: ['style', 'css'] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
       // Bootstrap 3 jquery requirement
-      { test:/bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/, loader: 'imports?jQuery=jquery' }
-    ]
-  }
+      { test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+        loader: 'imports?jQuery=jquery' },
+    ],
+  },
 }
